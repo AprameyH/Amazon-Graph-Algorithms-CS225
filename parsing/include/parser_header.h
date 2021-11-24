4d8fe0e2b8ef890e23e85b71c5307e5a863568b4
Apramey Hosahalli <apramey2@siebl-0224-42.ews.illinois.edu>
2021-11-24 14:40:22 -0600

    Fixed parser

--- parsing/include/parser_header.h
+++ parsing/include/parser_header.h
@@ -1,3 +1,22 @@
 #pragma once
 
 #include "node.h"
+#include <boost/algorithm/string.hpp>
+#include <fstream>
+#include <iostream>
+#include <map>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+namespace Parser {
+class ParserHeader {
+public:
+  void readData(string filename);
+  map<int, vector<Node>> getNeighbors();
+
+private:
+  map<int, vector<Node>> neighborNodes;
+};
+} // namespace Parser
